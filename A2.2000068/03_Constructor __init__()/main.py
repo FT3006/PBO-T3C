a9effdc56ee8827ef3337a319bfad6499e40da16
Wannn13 <muhammad.ridwan2552@gmail.com>
2021-10-21 05:42:40 +0700

    C:/Program Files/Git/03_Constructor __init__()

--- A2.2000068/03_Constructor __init__()/main.py
+++ A2.2000068/03_Constructor __init__()/main.py
@@ -1,17 +1,15 @@
-class Hero: #template
+class Hero:
 
-    def __init__(self, inputName, inputHealth, inputPower, inputArmor):
-      self.name = inputName
-      self.health = inputHealth
-      self.power = inputPower
-      self.armor = inputArmor
+    def __init__(self,inputName, inputHealth, inputPower, inputArmor):
+        self.name = inputName
+        self.health = inputHealth
+        self.power = inputHealth
+        self.armor = inputArmor
 
-
-hero1 = Hero("Gusion", 100, 100, 50)
-hero2 = Hero("Ling", 100, 50, 100)
-hero3 = Hero("Hayabusa", 100, 80, 100)
+hero1 = Hero("Gusion",100, 10, 4)
+hero2 = Hero("Ling",100, 15, 1)
+hero3 = Hero("Hayabusa",1000, 100, 0)
 
 print(hero1.__dict__)
-print(hero2.power)
-print(hero3.armor)
-
+print(hero2.armor)
+print(hero3.health) 
