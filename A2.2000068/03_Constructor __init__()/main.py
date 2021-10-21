ca93deffe24f4ff4ffe40a3f7061087683e907c2
MuhamadRidwanPermana <a2.2000068@mhs.stmik-sumedang.ac.id>
2021-10-21 07:04:57 +0700

    03_Constructor __init__()

--- A2.2000068/03_Constructor __init__()/main.py
+++ A2.2000068/03_Constructor __init__()/main.py
@@ -1,14 +1,14 @@
-class Hero:
+class HeroMobileLegends:
 
     def __init__(self,inputName, inputHealth, inputPower, inputArmor):
         self.name = inputName
         self.health = inputHealth
-        self.power = inputHealth
+        self.power = inputPower
         self.armor = inputArmor
 
-hero1 = Hero("Gusion",100, 10, 4)
-hero2 = Hero("Ling",100, 15, 1)
-hero3 = Hero("Hayabusa",1000, 100, 0)
+hero1 = HeroMobileLegends("Gusion",100, 10, 5)
+hero2 = HeroMobileLegends("Ling",100, 15, 8)
+hero3 = HeroMobileLegends("Hayabusa",100, 20, 10)
 
 print(hero1.__dict__)
 print(hero2.armor)
