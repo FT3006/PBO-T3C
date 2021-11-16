d57caa7e5e5bb9f21959672b59cec939d9b84d80
A22000102 <a2.2000102@mhs.stmik-sumedang.ac.id>
2021-11-16 13:19:33 +0700

    04_Class dan instance attributes

--- A2.2000102/04_Class dan instance attributes/main.py
+++ A2.2000102/04_Class dan instance attributes/main.py
@@ -12,9 +12,9 @@ class Hero : #template
     Hero.jumlah += 1
     print("membuat Hero dengan nama " + inputName)
 
-hero1 = Hero("Gusion",100, 10, 5)
+hero1 = Hero("zilong",100, 10, 5)
 print(Hero.jumlah)
-hero2 = Hero("Ling",100, 15, 8)
+hero2 = Hero("miya",100, 15, 8)
 print(Hero.jumlah)
-hero3 = Hero("Hayabusa",100, 20, 10)
+hero3 = Hero("nana",100, 20, 10)
 print(Hero.jumlah)
