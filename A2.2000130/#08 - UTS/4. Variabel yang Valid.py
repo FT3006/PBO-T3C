48615e66446969c2a4343dd25f2a57a57a47c033
A22000130 <A2.2000130@mhs.stmik-sumedang.ac.id>
2021-11-16 11:17:23 +0700

    A2.2000130

--- A2.2000130/#08 - UTS/4. Variabel yang Valid.py
+++ A2.2000130/#08 - UTS/4. Variabel yang Valid.py
@@ -1,16 +1,4 @@
-#.nama_bahasa = "python"
-nama_bahasa = "phyton"
-
-P3MBU4T = "Guido van Rossum" # nama variabel yang 'alay', tapi apakah valid? - (ya, valid)
-
-#T@hun_dibu@tny@ = 1991
-Tahun_dibuatnya = 1991
+P3MBU4T = "Guido van Rossum" # nama variabel yang 'alay', tapi apakah valid?
 
 # variabel panjang nih
 contoh_perusahaan_yang_menggunakan_bahasa_python = "Youtube, Path, dan lain-lain."
-
-#$belajar_python_oop = True
-belajar_phyton_oop = True
-
-#3m4ng_s3ru = "Benar"
-Em4ng_s3ru = "Benar"
