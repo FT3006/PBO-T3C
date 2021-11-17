4da1aeaa18632c8d3fcfb4a7e9b6305dc68bb3bb
MuhamadRidwanPermana <91520890+MuhamadRidwanPermana@users.noreply.github.com>
2021-11-17 14:51:15 +0700

    Update Main.py

--- A2.2000068/08_UTS/6. Membuat Objek/Main.py
+++ A2.2000068/08_UTS/6. Membuat Objek/Main.py
@@ -1,4 +1,9 @@
-class Kendaraan(object):
-  bahan_bakar = "bensin"
-mobil = Kendaraan()
-print(mobil.bahan_bakar)
+class Kendaraan:
+  bahan_bakar = "Bensin"
+
+  def __init__(self, kendaraan):
+    self.kendaraan = kendaraan
+
+mobil = Kendaraan ("Mobil")
+
+print("Bahan bakar mobil adalah " + mobil.bahan_bakar)
