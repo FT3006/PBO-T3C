ac50d477ba474fc16ecf70117a8248c001ee3fa8
MuhamadRidwanPermana <a2.2000068@mhs.stmik-sumedang.ac.id>
2022-01-06 13:28:12 +0700

    Pemulihan Jawaban UTS No 6. Membuat Objek

--- A2.2000068/08_UTS/6. Membuat Objek/Main.py
+++ A2.2000068/08_UTS/6. Membuat Objek/Main.py
@@ -1,2 +1,9 @@
-mobil = Kendaraan()
-print('bahan bakar mobil adalah' + mobil.bahan_bakar)
+class Kendaraan:
+  bahan_bakar = "Bensin"
+
+  def __init__(self, kendaraan):
+    self.kendaraan = kendaraan
+
+mobil = Kendaraan ("Mobil")
+
+print("Bahan bakar mobil adalah " + mobil.bahan_bakar)
